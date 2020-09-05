ddea199facaae3354f07e36bcb68e46ef37f993f
raflyid <raflyid@hotmail.com>
2020-09-05 20:57:45 +0700

    fix 403 page

--- application/views/auth/blocked.php
+++ application/views/auth/blocked.php
@@ -1,96 +1,70 @@
+
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
+  <meta charset="UTF-8">
+  <meta content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no" name="viewport">
+  <title>403 &mdash; Stisla</title>
 
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="">
-    <meta name="author" content="">
+  <!-- General CSS Files -->
+  <link rel="stylesheet" href="<?= base_url('vendor/dist/'); ?>assets/modules/bootstrap/css/bootstrap.min.css">
+  <link rel="stylesheet" href="<?= base_url('vendor/dist/'); ?>assets/modules/fontawesome/css/all.min.css">
 
-    <title>Access Blocked</title>
+  <!-- CSS Libraries -->
 
-    <!-- Custom fonts for this template-->
-    <link href="<?= base_url('assets/') ?>vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
-    <link
-        href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
-        rel="stylesheet">
+  <!-- Template CSS -->
+  <link rel="stylesheet" href="<?= base_url('vendor/dist/'); ?>assets/css/style.css">
+  <link rel="stylesheet" href="<?= base_url('vendor/dist/'); ?>assets/css/components.css">
+<!-- Start GA -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-94034622-3"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
 
-    <!-- Custom styles for this template-->
-    <link href="<?= base_url('assets/') ?>css/sb-admin-2.min.css" rel="stylesheet">
+  gtag('config', 'UA-94034622-3');
+</script>
+<!-- /END GA --></head>
 
-</head>
-
-<body id="page-top">
-
-    <!-- Page Wrapper -->
-    <div id="wrapper">
-
-        <!-- Content Wrapper -->
-        <div id="content-wrapper" class="d-flex flex-column bg-white">
-
-            <!-- Main Content -->
-            <div id="content">
-
-                <!-- Begin Page Content -->
-                <div class="container-fluid mt-5">
-
-                    <!-- 404 Error Text -->
-                    <div class="text-center">
-                        <div class="error mx-auto" data-text="403">403</div>
-                        <p class="lead text-gray-800 mb-5">Access Forbidden</p>
-                        <p class="text-gray-500 mb-0">It looks like you found a glitch in the matrix...</p>
-                        <a href="<?= base_url('auth/logout') ?>">&larr; Back to Dashboard</a>
-                    </div>
-
-                </div>
-                <!-- /.container-fluid -->
-
+<body>
+  <div id="app">
+    <section class="section">
+      <div class="container mt-5">
+        <div class="page-error">
+          <div class="page-inner">
+            <h1>403</h1>
+            <div class="page-description">
+            	You do not have access to this page.
             </div>
-            <!-- End of Main Content -->
-
-        </div>
-        <!-- End of Content Wrapper -->
-
-    </div>
-    <!-- End of Page Wrapper -->
-
-    <!-- Scroll to Top Button-->
-    <a class="scroll-to-top rounded" href="#page-top">
-        <i class="fas fa-angle-up"></i>
-    </a>
-
-    <!-- Logout Modal-->
-    <div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
-        aria-hidden="true">
-        <div class="modal-dialog" role="document">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title" id="exampleModalLabel">Ready to Leave?</h5>
-                    <button class="close" type="button" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">×</span>
-                    </button>
-                </div>
-                <div class="modal-body">Select "Logout" below if you are ready to end your current session.</div>
-                <div class="modal-footer">
-                    <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
-                    <a class="btn btn-primary" href="login.html">Logout</a>
-                </div>
+            <div class="page-search">
+              <div class="mt-3">
+                <a href="<?= base_url('partner') ?>">Back to Home</a>
+              </div>
             </div>
+          </div>
         </div>
-    </div>
+        <div class="simple-footer mt-5">
+          Copyright &copy; Smart Shopping 2020
+        </div>
+      </div>
+    </section>
+  </div>
 
-    <!-- Bootstrap core JavaScript-->
-    <script src="<?= base_url('assets/') ?>vendor/jquery/jquery.min.js"></script>
-    <script src="<?= base_url('assets/') ?>vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+<!-- General JS Scripts -->
+  <script src="<?= base_url('vendor/dist/'); ?>assets/modules/jquery.min.js"></script>
+  <script src="<?= base_url('vendor/dist/'); ?>assets/modules/popper.js"></script>
+  <script src="<?= base_url('vendor/dist/'); ?>assets/modules/tooltip.js"></script>
+  <script src="<?= base_url('vendor/dist/'); ?>assets/modules/bootstrap/js/bootstrap.min.js"></script>
+  <script src="<?= base_url('vendor/dist/'); ?>assets/modules/nicescroll/jquery.nicescroll.min.js"></script>
+  <script src="<?= base_url('vendor/dist/'); ?>assets/modules/moment.min.js"></script>
+  <script src="<?= base_url('vendor/dist/'); ?>assets/js/stisla.js"></script>
+  
+  <!-- JS Libraies -->
 
-    <!-- Core plugin JavaScript-->
-    <script src="<?= base_url('assets/') ?>vendor/jquery-easing/jquery.easing.min.js"></script>
-
-    <!-- Custom scripts for all pages-->
-    <script src="<?= base_url('assets/') ?>js/sb-admin-2.min.js"></script>
-
+  <!-- Page Specific JS File -->
+  
+  <!-- Template JS File -->
+  <script src="<?= base_url('vendor/dist/'); ?>assets/js/scripts.js"></script>
+  <script src="<?= base_url('vendor/dist/'); ?>assets/js/custom.js"></script>
 </body>
-
 </html>
